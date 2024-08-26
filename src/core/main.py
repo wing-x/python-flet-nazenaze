184129b7203cb7f7d15409859c9576ea73d63833
LAPTOP-M32GGE7N <sisakux@gmail.com>
2024-08-26 09:42:32 +0900

    フォルダ構成の変更、ユニットテストが機能するかの確認用のコード追加

--- src/core/main.py
+++ src/core/main.py
@@ -1,4 +1,7 @@
 # これはサンプルの Python スクリプトです。
+import json
+import math
+import re
 
 # Shift+F10 を押して実行するか、ご自身のコードに置き換えてください。
 # Shift を2回押す を押すと、クラス/ファイル/ツールウィンドウ/アクション/設定を検索します。
@@ -9,8 +12,21 @@ def print_hi(name):
     print(f"Hi, {name}")  # Ctrl+F8を押すとブレークポイントを切り替えます。
 
 
+def calc(a, b):
+    return a + b
+
+
 # ガター内の緑色のボタンを押すとスクリプトを実行します。
 if __name__ == "__main__":
-    print_hi("PyCharm")
-
+    # print_hi("PyCharm")
+    # tes = ["aaa", "bbb"]
+    # tes = ",".join(tes).title()
+    # print(tes)
+    print(f"{3.14:3.1f}")
+    tes = "abc123xyz"
+    print(re.match("x.z", tes))
+    print(re.search("x.z", tes))
+    json_str = '{"a":123}'
+    tes = json.loads(json_str)
+    print(tes["a"])
 # PyCharm のヘルプは https://www.jetbrains.com/help/pycharm/ を参照してください
---
+++ tests/core/test_main.py
@@ -0,0 +1,10 @@
+import main
+
+
+def test_print_hi():
+    assert True
+
+
+def test_calc():
+    num = main.calc(1, 2)
+    assert num == 3
